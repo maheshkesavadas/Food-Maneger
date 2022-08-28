e2db1287e358dece4ccd1d9aa80061808f899b28
maheshkesavadas <maheshkesavadas123@gmail.com>
2022-08-28 15:26:38 +0530

    my Food App manager

---
+++ src/context/initialState.js
@@ -0,0 +1,17 @@
+import {
+  fetchCart,
+  fetchTotal,
+  fetchUser,
+} from "../utils/fetchLocalStorageData";
+
+const userInfo = fetchUser();
+const cartInfo = fetchCart();
+const total    = fetchTotal();
+
+export const initialState = {
+  user: userInfo,
+  cartItems: cartInfo,
+  total: [],
+  foodItems: null,
+};
+
---
+++ src/utils/data.js
@@ -0,0 +1,90 @@
+export const categories = [
+  {
+    id: 1,
+    name: "Chicken",
+    urlParamName: "chicken",
+  },
+  {
+    id: 2,
+    name: "Beef",
+    urlParamName: "beef",
+  },
+  {
+    id: 3,
+    name: "Mutton",
+    urlParamName: "mutton",
+  },
+  {
+    id: 4,
+    name: "Fish",
+    urlParamName: "fish",
+  },
+  {
+    id: 5,
+    name: "Pizza",
+    urlParamName: "pizza",
+  },
+  {
+    id: 6,
+    name: "Snack",
+    urlParamName: "snacks",
+  },
+  {
+    id: 7,
+    name: "Deserts",
+    urlParamName: "deserts",
+  },
+  {
+    id: 8,
+    name: "Soft Drinks",
+    urlParamName: "drinks",
+  },
+];
+
+export const foodItemsStatic = [
+  {
+    id: "1647820416809",
+    calories: "120",
+    category: "chicken",
+    imageURL:
+      "",
+    price: "10",
+    title: "Chicken Kebab",
+  },
+  {
+    id: "1647820581313",
+    calories: "100",
+    category: "chicken",
+    imageURL:
+      "",
+    price: "8.5",
+    title: "Spicy Chilly Cheese Pasta",
+  },
+  {
+    id: "1647820625440",
+    calories: "60",
+    category: "chicken",
+    imageURL:
+      "",
+    price: "12.5",
+    title: "Pepper Max Noodles",
+  },
+  {
+    id: "1647821206472",
+    calories: "80",
+    category: "chicken",
+    imageURL:
+      "",
+    price: "8.5",
+    title: "Chicken Fried Rice",
+  },
+  {
+    id: "1647821596820",
+    calories: "100",
+    category: "fish",
+    imageURL:
+      "",
+    price: "12.5",
+    title: "Overnight Marinated Fish",
+  },
+];
